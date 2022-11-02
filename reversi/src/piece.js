5809d6b902feca695c0910ae32b487821328b577
jkhbuild <jkh.build@gmail.com>
2022-11-02 13:48:52 -0400

    piece completed

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -1,19 +1,26 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+  this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  if (this.color === "black") {
+    return "white";
+  } else {
+    return "black";
+  }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  this.color = this.oppColor();
 };
 
 /**
@@ -21,10 +28,15 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  if (this.color === "black") {
+    return "B";
+  } else {
+    return "W";
+  }
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
+if (typeof window === "undefined") {
+  module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
